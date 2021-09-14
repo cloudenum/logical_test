1c677c4fc3f90d9a9198cdbdc435d38cc3e92437
cloudenum <23111954+cloudenum@users.noreply.github.com>
2021-09-14 10:51:38 +0700

    query update

--- 2_mysql_query.sql
+++ 2_mysql_query.sql
@@ -1,5 +1,4 @@
 SELECT mhs_nama FROM tb_mahasiswa_nilai
     INNER JOIN tb_mahasiswa ON tb_mahasiswa.mhs_id = tb_mahasiswa_nilai.mhs_id
     INNER JOIN tb_matakuliah ON tb_matakuliah.mk_id = tb_mahasiswa_nilai.mk_id
-    WHERE mk_kode = "MK303" ORDER BY DESC LIMIT 1;
-    
+    WHERE mk_kode = "MK303" ORDER BY nilai DESC LIMIT 1;
